870c6ceba6db69c5077ec43f35199bffd3fbbc15
absiddik676 <absiddik8200@gmail.com>
2023-05-02 22:22:44 +0600

    footer design done

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import Navbar from '../components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../share/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar/>
             <Outlet/>
-
+            <Footer/>
         </div>
     );
 };
--- src/components/Chef/ShowChefData/ShowChefData.jsx
+++ src/components/Chef/ShowChefData/ShowChefData.jsx
@@ -6,7 +6,7 @@ const ShowChefData = ({ chef }) => {
     const { chefPicture, chefName, yearsOfExperience, likes ,numberOfRecipes} = chef;
     return (
         <div>
-            <div className="w-full max-w-sm mx-auto bg-white rounded-md shadow-md overflow-hidden">
+            <div className="w-full max-w-sm mx-auto m-5 bg-white rounded-md shadow-md overflow-hidden">
                 <img className="w-full h-48 object-cover object-center" src={chefPicture} alt="Chef Image" />
                 <div className="p-4">
                     <div className="flex justify-between items-center">
@@ -14,9 +14,13 @@ const ShowChefData = ({ chef }) => {
                         <span className="bg-green-500 text-white py-1 px-2 rounded-full text-sm">{yearsOfExperience} years experience</span>
                     </div>
                     <p className="mt-3 text-gray-600"></p>
-                    <div className="flex justify-end mt-4">
-                        <p>Total Recipes</p>
-                        <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors duration-300">Learn More</button>
+                    <div className="flex justify-between mt-4 py-4">
+                        <p className='text-lg font-semibold'>Total Recipes {numberOfRecipes}</p>   
+                        <p className='text-lg font-semibold'>Total Likes {likes}</p> 
+                    </div>
+                    <div className="flex justify-center mt-4">
+                        
+                        <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors duration-300">View Recipes</button>
                     </div>
                 </div>
             </div>
